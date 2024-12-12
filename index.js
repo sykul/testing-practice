66c009e3f9b457858f5f46921fb2c69ab49449c0
sykul <luke.flatley@outlook.com>
2024-12-12 11:49:50 +0100

    function to reverse string

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ function capitalise(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export {capitalise}
+function reverseString(str) {
+    let arr = str.split('');
+    arr.reverse();
+    arr = arr.reduce((string, character) => string + character, '')
+    return arr;
+}
+
+export { capitalise, reverseString }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-import { capitalise } from "."
+import { capitalise, reverseString } from "."
 
 test('first letter is capital', () => {
     expect(capitalise('tadsfasdf')).toEqual('Tadsfasdf')
+})
+
+test('a string is returned reversed', () => {
+    expect(reverseString('blah')).toEqual('halb')
 })
